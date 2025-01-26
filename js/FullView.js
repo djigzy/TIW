d906ff05264a52242f1fbf71924f256f7ea7048a
Djigzy <87614312+djigzy@users.noreply.github.com>
2025-01-26 23:52:53 +0100

    Delete js directory

--- js/FullView.js
+++
@@ -1,7 +0,0 @@
-function FullView(ImgLink) {
-    document.getElementById("FullImage").src = ImgLink;
-    document.getElementById("FullImageView").style.display = "flex";
-}
-function CloseFullView() {
-    document.getElementById("FullImageView").style.display = "none";
-}
